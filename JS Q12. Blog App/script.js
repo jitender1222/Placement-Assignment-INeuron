d4c546facae6c0b7aa45f379e8bc88b110288580
jitender singh <jeetusinghkanyal@gmail.com>
2023-05-23 11:44:37 +0530

    added blog post by uisng the given API

---
+++ JS Q12. Blog App/script.js
@@ -0,0 +1,63 @@
+const container = document.querySelector(".blog-container");
+
+const blogArr = [];
+
+const onSubmit = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+  const data = await res.json();
+
+  console.log(data);
+
+  data.map((element, index) => {
+    if (index > 25) return;
+
+    blogArr.push(element);
+    container.insertAdjacentHTML(
+      "afterbegin",
+      `<div class="w-[250px] min-h-[250px] flex flex-col justify-between  items-center bg-[#ffffff] p-4 rounded-md drop-shadow-md">
+      <h2 class="text-[#fca311] font-serif text-xl text-center">${element.title}</h2>
+      <p class="text-center text-[#14213d]">${element.body}</p>
+    </div>`
+    );
+    // <button class="!btn bg-[#e07a5f] px-4 py-2 rounded text-white hover:bg-[#be6147] transition-all duration-200" id="btn">Delete</button>
+  });
+};
+onSubmit();
+
+const titleInput = document.getElementById("title");
+const textInput = document.getElementById("text");
+const userBlogContainer = document.querySelector(".my-blog");
+const hideText = document.querySelector(".hide");
+const deleteButton = document.querySelector(".btn");
+
+const addData = async (event) => {
+  event.preventDefault();
+
+  if (!titleInput.value && !textInput.value) {
+    alert("Please Fill the data properly");
+  }
+
+  hideText.classList.add("hidden");
+
+  deleteButton.classList.remove("hidden");
+
+  const title = titleInput.value;
+  const text = textInput.value;
+
+  userBlogContainer.insertAdjacentHTML(
+    "afterbegin",
+    `
+  <div class="w-[250px] min-h-[250px] flex flex-col justify-between items-center bg-[#ffffff] p-4 rounded-md drop-shadow-md">
+      <h2 class="text-[#fca311] font-serif text-xl text-center">${title}</h2>
+      <p class="text-center text-[#14213d]">${text}</p>
+    </div>
+  `
+  );
+
+  titleInput.value = "";
+  textInput.value = "";
+};
+
+deleteButton.addEventListener("click", () => {
+  location.reload();
+});
