8430b9fdcb4e183945b0b89ff1981c99a3eee903
jitender singh <jeetusinghkanyal@gmail.com>
2023-06-06 17:48:47 +0530

    added value attribute

--- React Q16 Task Manager/myapp/src/components/Login.js
+++ React Q16 Task Manager/myapp/src/components/Login.js
@@ -3,55 +3,71 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 function Login() {
-    const navigate = useNavigate('');
-    const [userEmail, setUserEmail] = useState('');
-    const [userPassword, setUserPassword] = useState('');
+  const navigate = useNavigate("");
+  const [userEmail, setUserEmail] = useState("eve.holt@reqres.in");
+  const [userPassword, setUserPassword] = useState("cityslicka");
+  const submitData = async () => {
+    const data = { email: userEmail, password: userPassword };
 
-    const submitData = async () => {
-        const data = { email: userEmail, password: userPassword }
+    await axios
+      .post("https:reqres.in/api/register", data)
+      .then((resp) => {
+        return resp.data;
+      })
+      .then((result) => {
+        localStorage.setItem("id", result.id);
+        localStorage.setItem("token", result.token);
+        navigate("/dashboard");
+      })
 
-        await axios.post('https:reqres.in/api/register', data)
-            .then(resp => { return resp.data })
-            .then((result) => {
-                localStorage.setItem('id', result.id)
-                localStorage.setItem('token', result.token)
-                navigate('/dashboard')
-            })
+      .catch((err) => console.log(err));
+  };
+  const loginUser = async (e) => {
+    e.preventDefault();
+    submitData();
 
-            .catch((err) => console.log(err))
-    }
-    const loginUser = async (e) => {
-        e.preventDefault()
-        submitData();
+    setUserEmail("");
+    setUserPassword("");
+  };
 
-        setUserEmail('');
-        setUserPassword('');
-    }
+  return (
+    <>
+      <div className="task-app">
+        <h1>Task Manager</h1>
+      </div>
+      <div className="topbar">
+        <h2>Welcome Guest</h2>
+        <p>Please login to add task</p>
+      </div>
 
-
-
-    return (
-        <>
-            <div className="task-app">
-                <h1>Task Manager</h1>
-            </div>
-            <div className="topbar">
-                <h2>Welcome Guest</h2>
-                <p>Please login to add task</p>
-            </div>
-
-            <div className='login-div'>
-                <div className="form-div">
-                    <h1>Login</h1>
-                    <form className='login-form' action="">
-                        <input type="email" name="email" id="email" placeholder='Email' onChange={(e) => { setUserEmail(e.target.value) }} />
-                        <input type="password" name="password" id="password" placeholder='Password' onChange={(e) => { setUserPassword(e.target.value) }} />
-                        <button type="submit" onClick={loginUser}>Submit</button>
-                    </form>
-                </div>
-            </div>
-        </>
-    )
+      <div className="login-div">
+        <div className="form-div">
+          <h1>Login</h1>
+          <form className="login-form" action="">
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={userEmail}
+              placeholder="Email"
+              onChange={(e) => setUserEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={userPassword}
+              placeholder="Password"
+              onChange={(e) => setUserPassword(e.target.value)}
+            />
+            <button type="submit" onClick={loginUser}>
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Login;
